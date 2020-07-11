1c48e738a3035640b20e0e7ee93406c52b2525c3
ycodex <karthiknaik120@gmail.com>
2020-07-11 22:09:49 +0530

    news major update

--- main.py
+++ main.py
@@ -2,18 +2,40 @@ import tweepy as tp
 from api import main
 import os
 from dotenv import load_dotenv,find_dotenv
+import news
+import random
 
-load_dotenv("keys.env")
-consumer_key=str(os.getenv("consumer_key"))
-consumer_secret=str(os.getenv("consumer_secret"))
-access_token=str(os.getenv("access_token"))
-access_token_secret=str(os.getenv("access_token_secret"))
 
-auth=tp.OAuthHandler(consumer_key,consumer_secret)
-auth.set_access_token(access_token, access_token_secret) 
-api=tp.API(auth)
 
-tweet=main()
 
-api.update_status(tweet)
+def auth():
+    load_dotenv("keys.env")
+    consumer_key=str(os.getenv("consumer_key"))
+    consumer_secret=str(os.getenv("consumer_secret"))
+    access_token=str(os.getenv("access_token"))
+    access_token_secret=str(os.getenv("access_token_secret"))
 
+    auth=tp.OAuthHandler(consumer_key,consumer_secret)
+    auth.set_access_token(access_token, access_token_secret) 
+    
+    api=tp.API(auth)
+    return api
+
+
+
+def choose(key):
+    option=random.randint(0,1)
+    if(option==0):
+        tweet=main()
+        key.update_status(tweet)
+    else:
+        d,t=news.req()
+        if(news.read_and_compare(d)):
+            tweets=news.post(t)
+            key.update_status(tweets)
+
+
+
+if __name__=='__main__':
+    apis=auth()
+    choose(apis)
---
+++ news.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup
+import requests
+import csv 
+
+
+date=0
+lis=[]
+def req():
+    URL = "https://www.mohfw.gov.in/"
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.content, 'html5lib') 
+     
+    table = soup.find('div', attrs = {'class':'row equal-height'}) 
+    table2= table.find('div', attrs = {'class':'col-lg-4 col-md-4 col-sm-6 col-xs-12'}) 
+    for s in table2.find("strong"):
+        date=s
+    return date,table2
+    
+    
+                         
+
+def read_and_compare(date):
+
+    with open('db.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, skipinitialspace=True)
+        lis=[]
+        for row in spamreader:
+            lis.append(row)
+            if(date in row):
+                return 0
+            
+        write(date,len(lis)-2)
+        return 1 
+        
+        
+
+   
+def write(date,length):
+    with open('db.csv','a', newline='') as csvfile:
+        
+        writer=csv.writer(csvfile)
+        length+=2
+        writer.writerow([length,date])
+        
+        
+def post(table2):
+    s=""
+    s+='Latest updates from MOHFW\n'
+    s+="-------------------------\n"
+    s+=str(table2.find("a").contents[0])
+    s+="\n"
+    s+=table2.find("a")["href"]
+    return s
+
+
+# d,t=req()
+# if(read_and_compare(d)):
+#     tweets=post(t)
+#     print(tweets)
+        
+        
+        
+
+
