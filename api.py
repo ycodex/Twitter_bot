c504ff694568e58cce0ff20db2023e5b7490639a
ycodex <karthiknaik120@gmail.com>
2020-07-11 14:53:47 +0530

    api updated

--- api.py
+++ api.py
@@ -2,13 +2,13 @@ import requests
 from operator import itemgetter 
 
 def main():
-    res=requests.get("http://covid19-india-adhikansh.herokuapp.com/states")
+    res=requests.get("https://covid19-mohfw.herokuapp.com/states")
     
     if res.status_code!=200:
         raise Exception("ERROR:unsuccessful")
     data=res.json()
-    data=data["state"]
-    sorted_data=(sorted(data, key=itemgetter('active'),reverse=True))
+    data=data["states"]
+    sorted_data=(sorted(data, key=itemgetter('cases'),reverse=True))
     #return sorted_data
     s="Current  TOP states:\n"
     s+="\n"
@@ -18,16 +18,16 @@ def main():
     #print("Current  TOP states:\n")
     
     for i in range(3):
-        name=sorted_data[i]['name']
-        total=sorted_data[i]['total']
-        active=sorted_data[i]['active']
-        cured=sorted_data[i]['cured']
-        death=sorted_data[i]['death']
+        name=sorted_data[i]['state']
+        total=sorted_data[i]['cases']
+        active=sorted_data[i]['recoveries']
+        cured=sorted_data[i]['deaths']
+        death=sorted_data[i]['total']
         s+="Name : "+str(name)+"\n"
-        s+="Total : "+str(total)+"\n"
-        s+="Active : "+str(active)+"\n"
-        s+="Cured : "+str(cured)+"\n"
-        s+="Death : "+str(death)+"\n"
+        s+="Cases : "+str(total)+"\n"
+        s+="Recoveries : "+str(active)+"\n"
+        s+="Deaths : "+str(cured)+"\n"
+        s+="Total : "+str(death)+"\n"
         
         # print(f"Name : {name}")
         # print(f"Total : {total}")
